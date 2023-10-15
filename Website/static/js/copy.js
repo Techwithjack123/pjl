d92dc2d54ba38aab68bb9096df93afee236761d2
Jack Masters <105387397+jack-masters@users.noreply.github.com>
2023-10-15 20:34:36 +0100

    Delete Website directory

--- Website/static/js/copy.js
+++
@@ -1,6 +0,0 @@
-function copy() {
-        navigator.clipboard.writeText('loadstring(game:HttpGet("https://raw.githubusercontent.com/ProjectLightningDev/Project-Lightning-Loader/main/Loader.Lua"))()');
-    alert("Copied!");
-}
-
-document.getElementById("copy-btn").addEventListener("click", copy);
